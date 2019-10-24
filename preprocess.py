5ecd6ea742766ccabd3212bb574ab9748a684773
Thejas-bhat <thejasbhat.6@gmail.com>
2019-10-24 21:42:45 +0530

    small changes

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,7 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+
 
 def str_to_list(x):
     """
@@ -27,7 +27,6 @@ def preprocess_text(text):
     """
         Preprocess text data for performing analysis.
     """
-    # print(type(text))
     # Converting all the letters to lowercase
     text = text.lower()
     # Removing numbers from the text
@@ -50,7 +49,6 @@ def compute(data_dir="./data/", num_features=100):
     """
         Computes the TF-IDF vector and saves it.
         The TF-IDF model is also saved.
-
         Parameters
         ----------
         data_dir : string
@@ -73,7 +71,7 @@ def compute(data_dir="./data/", num_features=100):
 
     # Get each paper's abstract/summary
     paperSummaries = np.array(list(map(lambda x: x["summary"], data)))
-    idSummaryMap = list(map(lambda x:(x["id"], x["summary"]), data))
+
     cleanSummaries = np.array(list(map(preprocess_text, paperSummaries)))
     # TODO: Stem the words later if needed
 
@@ -81,47 +79,15 @@ def compute(data_dir="./data/", num_features=100):
     vectorizer = TfidfVectorizer(
         tokenizer=word_tokenize, max_features=num_features)
     vectSum = vectorizer.fit_transform(cleanSummaries)
-    print(len(vectSum.toarray()))
 
     np.save(data_dir + "tfidf-vectors-" + str(num_features) + ".npy", vectSum)
 
     print("Computed vector and saved!")
 
     with open(data_dir + 'vectorizer.pk', 'wb') as fin:
-            pickle.dump(vectorizer, fin)
+        pickle.dump(vectorizer, fin)
 
     print("Saved TF-IDF vectorizer!")
-
-    query = ["Recent approaches based on artificial neural networks (ANNs) have shown\npromising results for short-text classification. However, many short texts\noccur in sequences (e.g., sentences in a document or utterances in a dialog),\nand most existing ANN-based systems do not leverage the preceding short texts\nwhen classifying a subsequent one. In this work, we present a model based on\nrecurrent neural networks and convolutional neural networks that incorporates\nthe preceding short texts. Our model achieves state-of-the-art results on three\ndifferent datasets for dialog act prediction."]
-    query = np.array(query)
-    recommendedList = recommendation(query, vectorizer, vectSum, idSummaryMap)
-
-
-def recommendation(query, tf, tfidf_matrix, idSummariesMap):
-    """ 
-        Takes the query, performs basic nlp processing, 
-        fits it into the tfidf vector space model, 
-        returns a list of the most similar papers using the cosine similarity on the model.
-     """
-    # process the query 
-    processedQuery = np.array(list(map(preprocess_text, query)))
-    
-    tfidfQuery = tf.fit_transform(processedQuery)
-    cosine_similarities = linear_kernel(tfidfQuery, tfidf_matrix)
-    related_docs_indices = cosine_similarities[0].argsort()[:-10:-1]
-    result = []
-    for idx, row in enumerate(idSummariesMap):
-        # print(idx)
-        if(idx in related_docs_indices):
-            result.append(row[0])
-    
-    return result
-
-
-
-
-
-    
 
 
 if __name__ == "__main__":
