b80a2bf2e921f97751a5e34b225b5167b19626a7
Thejas-bhat <thejasbhat.6@gmail.com>
2019-10-25 19:56:05 +0530

    query length problem

--- premodel.py
+++ premodel.py
@@ -26,7 +26,7 @@ def recommendation(query, tf, tfidf_matrix, idSummariesMap):
     
     tfidfQuery = tf.fit_transform(processedQuery)
     cosine_similarities = linear_kernel(tfidfQuery, tfidf_matrix)
-    related_docs_indices = cosine_similarities[0].argsort()[:-10:-1]
+    related_docs_indices = cosine_similarities[0].argsort()[:-5:-1]
     result = []
     for idx, row in enumerate(idSummariesMap):
         # print(idx)
@@ -52,7 +52,8 @@ if __name__ == "__main__":
     # vectSum = np.load('./data/tfidf-vectors-100.npy', allow_pickle = True)
     vectSum = vectorizer.fit_transform(cleanSummaries)
     
-    query = np.array(["Recent approaches based on artificial neural networks (ANNs) have shown\npromising results for short-text classification. However, many short texts\noccur in sequences (e.g., sentences in a document or utterances in a dialog),\nand most existing ANN-based systems do not leverage the preceding short texts\nwhen classifying a subsequent one. In this work, we present a model based on\nrecurrent neural networks and convolutional neural networks that incorporates\nthe preceding short texts. Our model achieves state-of-the-art results on three\ndifferent datasets for dialog act prediction."], dtype=object)
+    
+    query = np.array(["Recent approaches Multiresolution Recurrent Neural Networks: An Application to Dialogue\n  Response Generation based on artificial neural networks (ANNs) have shown\npromising results for short-text classification.We report on the production experience of the D0 experiment at the Fermilab\nTevatron, using the SAM data handling system with a variety of computing\nhardware configurations, batch systems, and mass storage strategies. We have\nstored more than 300 TB of data in the Fermilab Enstore mass storage system. We\ndeliver data through this system at an average rate of more than 2 TB/day to\nanalysis programs, with a substantial multiplication factor in the consumed\ndata through intelligent cache management. We handle more than 1.7 Million\nfiles in this system and provide data delivery to user jobs at Fermilab on four\ntypes of systems: a reconstruction farm, a large SMP system, a Linux batch\ncluster, and a Linux desktop cluster. In addition, we import simulation data\ngenerated at 6 sites worldwide, and deliver data to jobs at many more sites. We\ndescribe the scope of the data handling deployment worldwide, the operational\nexperience with this system, and the feedback of that experience. A knowledge base is redundant if it contains parts that can be inferred from\nthe rest of it. We study the problem of checking whether a CNF formula (a set\nof clauses) is redundant, that is, it contains clauses that can be derived from\nthe other ones. Any CNF formula can be made irredundant by deleting some of its\nclauses: what results is an irredundant equivalent subset (I.E.S.) We study the\ncomplexity of some related problems: verification, checking existence of a\nI.E.S. with a given size, checking necessary and possible presence of clauses\nin I.E.S.'s, and uniqueness. We also consider the problem of redundancy with\ndifferent definitions of equivalence.However, many short texts\noccur in sequences (e.g., sentences in a document or utterances in a dialog),\nand most existing ANN-based systems do not leverage the preceding short texts\nwhen classifying a subsequent one. In this work, we present a model based on\nrecurrent neural networks and convolutional neural networks that incorporates\nthe preceding short texts. Our model achieves state-of-the-art results on three\ndifferent datasets for dialog act prediction."], dtype=object)
     recommendedList = recommendation(query, vectorizer, vectSum, idSummaryMap)
     
 
