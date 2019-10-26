39d588c8bdeaa1a799040b4a31ec6f13741738f6
Thejas-bhat <thejasbhat.6@gmail.com>
2019-10-26 21:55:18 +0530

    topic modelling the dataset

--- premodel.py
+++ premodel.py
@@ -2,11 +2,20 @@ import json
 import pickle
 import pprint
 
+import gensim 
 import numpy as np
 from sklearn.metrics.pairwise import linear_kernel
 
-from preprocess import preprocess_text
+from preprocess import preprocess_text, str_to_list
+from gensim.test.utils import common_corpus, common_dictionary
+from sklearn.decomposition import NMF, LatentDirichletAllocation
 
+
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 def recommendation(query, tfidf_model, tfidf_matrix, paperTitles):
     """
@@ -40,6 +49,23 @@ def recommendation(query, tfidf_model, tfidf_matrix, paperTitles):
 
     return result
 
+def possibleTopics(data, tf, vectSum):
+    """ 
+        Takes data and finds the possible topics of the papers
+        using Topic Modeling and Latent Dirichlet Allocation
+        
+     """
+    processedQuery = np.array(list(map(preprocess_text, query)))
+    tfidfQuery = tf.transform(processedQuery)
+
+    no_top_words = 10
+    tf_feature_names =  tf.get_feature_names()
+    processedfeatures = np.array(list(map(preprocess_text, tf_feature_names)))
+    
+    # print(tf_feature_names)
+    nmf = NMF(n_components=10, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(vectSum)
+    display_topics(nmf, processedfeatures, no_top_words)
+
 
 if __name__ == "__main__":
     data_dir = "./data/"
@@ -57,5 +83,7 @@ if __name__ == "__main__":
 
     query = np.array(["Manifold analysis and dimensionality reduction"], dtype=object)
     recommendedList = recommendation(query, vectorizer, vectSum, paperTitles)
-
-    pprint.pprint(recommendedList)
+    topics = possibleTopics(data, vectorizer, vectSum)
+    
+    # pprint.pprint(recommendedList)
+    pprint.pprint(topics)
